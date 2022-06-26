7ff6cd626797dcb7e4e69fecaf310c9f0e798948
Felipe David <felipedavid@gmail.com>
2022-06-26 19:55:08 -0300

    adding some route components

---
+++ src/routes/home/home.component.jsx
@@ -0,0 +1,37 @@
+import Directory from '../../components/directory/directory.component'
+
+const Home = () => {
+  const categories = [
+    {
+      id: 1,
+      title: "GPUs",
+      imageUrl: "https://cdn.dooca.store/107/products/dx6hr51rghkvbxr5tuw5zotjjjybw6km610b_1600x1600+fill_ffffff.png"
+    },
+    {
+      id: 2,
+      title: "CPUs",
+      imageUrl: "https://cdn.dooca.store/107/products/dx6hr51rghkvbxr5tuw5zotjjjybw6km610b_1600x1600+fill_ffffff.png"
+    },
+    {
+      id: 3,
+      title: "Memory",
+      imageUrl: "https://cdn.dooca.store/107/products/dx6hr51rghkvbxr5tuw5zotjjjybw6km610b_1600x1600+fill_ffffff.png"
+    },
+    {
+      id: 4,
+      title: "Cases",
+      imageUrl: "https://cdn.dooca.store/107/products/dx6hr51rghkvbxr5tuw5zotjjjybw6km610b_1600x1600+fill_ffffff.png"
+    },
+    {
+      id: 5,
+      title: "Other",
+      imageUrl: "https://cdn.dooca.store/107/products/dx6hr51rghkvbxr5tuw5zotjjjybw6km610b_1600x1600+fill_ffffff.png"
+    },
+  ];
+
+  return (
+    <Directory categories={categories}/>
+  );
+}
+
+export default Home;
