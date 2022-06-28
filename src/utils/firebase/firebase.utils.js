a6350f844b8b5208253d515ab160241d8e141a1d
Felipe David <felipedavid@gmail.com>
2022-06-28 06:31:19 -0300

    authorization with firebase

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,21 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBE4ROC0PilDl6mwOAtQnBzwHmk348QGiQ",
+  authDomain: "byte-shop-dc8a8.firebaseapp.com",
+  projectId: "byte-shop-dc8a8",
+  storageBucket: "byte-shop-dc8a8.appspot.com",
+  messagingSenderId: "12990828420",
+  appId: "1:12990828420:web:aeb01e9a99a46fe46ad7d1"
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+    prompt: "select_account"
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
